eabfd021d920fca424fb5949a38d5f3af86dc117
crowrooster971 <wanlishajna@hotmail.com>
2025-08-30 09:00:12 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Système de Collecte de Retours Utilisateurs</h1>
+      {/* Ici, nous ajouterons des composants pour le formulaire et le tableau des retours */}
+    </div>
+  );
+}
+
+export default App;
